960fd0b78d575c8cdcf9321e751450a654f628a4
HoangTheViet <theviet050804@gmail.com>
2024-09-25 16:57:15 +0300

    Second commit

--- src/attacks/SludgeBomb.java
+++ src/attacks/SludgeBomb.java
@@ -14,4 +14,9 @@ public class SludgeBomb extends SpecialMove {
     protected void applyOppEffects(Pokemon p) {
         if(Math.random() <= 0.3) Effect.poison(p);
     }
+
+    @Override
+    public String describe() {
+        return "Uses Sludge Bomb";
+    }
 }
